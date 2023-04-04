dfc781d9ad39bcff8c5b3f39021ae1017a47bdaa
Deepti-Jethe <jethedeepti@gmail.com>
2023-04-04 18:23:26 +0530

    commit1

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import trollface from "../images/troll-face.png";
+
+export default function Header() {
+  return (
+    <header className="header">
+      <img src={trollface} alt="troll-face" className="header--image" />
+      <h2 className="header--title">Meme Generator</h2>
+      <h4 className="header--project">React Project</h4>
+    </header>
+  );
+}
